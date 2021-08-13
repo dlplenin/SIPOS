086c832c938f2e9df5f7b3362776fbbf5542549e
Diego Pardo <dlplenin@hotmail.com>
2021-08-12 21:48:43 -0500

    Add user-rol

--- SIPOS/Persistence/SiposContext.cs
+++ SIPOS/Persistence/SiposContext.cs
@@ -12,6 +12,12 @@ namespace SIPOS.Persistence
             optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=SIPOS;Integrated Security=True");
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SiposUserRol>()
+                .HasKey(ur => new { ur.SiposRolId, ur.SiposUserId });
+        }
+
         public DbSet<SiposUser> SiposUser { get; set; }
         public DbSet<Product> Product { get; set; }
         public DbSet<Order> Order { get; set; }
